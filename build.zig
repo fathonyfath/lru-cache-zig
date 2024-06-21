858a59f2fd91e574a8b3bba2ad5fae8070e61953
fathonyfath <4829378+fathonyfath@users.noreply.github.com>
2024-06-21 17:58:50 +0900

    Add basic LRU cache contract

--- build.zig
+++ build.zig
@@ -23,14 +23,25 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const exe_unit_tests = b.addTest(.{
+    const test_step = b.step("test", "Run unit tests");
+
+    registerTest(b, test_step, .{
+        .name = "main",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    registerTest(b, test_step, .{
+        .name = "lru_cache",
+        .root_source_file = b.path("src/lru_cache.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+}
 
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
+fn registerTest(b: *std.Build, test_step: *std.Build.Step, options: std.Build.TestOptions) void {
+    const unit_tests = b.addTest(options);
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+    test_step.dependOn(&run_unit_tests.step);
 }
---
+++ src/lru_cache.zig
@@ -0,0 +1,42 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+
+/// Error definition for LRUCache
+pub const LRUCacheError = error{NoElementFound};
+
+/// Implementation of LRU Cache
+pub const LRUCache = struct {
+    allocator: Allocator,
+    capacity: usize,
+
+    const Self = @This();
+
+    /// Create the LRU Cache
+    pub fn init(allocator: Allocator, capacity: usize) Self {
+        return Self{
+            .allocator = allocator,
+            .capacity = capacity,
+        };
+    }
+
+    /// Destructor of LRU Cache
+    /// Call this to clean up the memory used by this object
+    pub fn deinit(self: *LRUCache) void {
+        _ = self;
+    }
+
+    /// Returns a value of key exists on the cache
+    /// If key is found on the cache, set the key-value pair to the top priority
+    pub fn get(self: *LRUCache, key: i32) LRUCacheError!i32 {
+        _ = self;
+        _ = key;
+    }
+
+    /// Add the key-value pair to the cache
+    /// If the key is already exists, update the value
+    pub fn put(self: *LRUCache, key: i32, value: i32) !void {
+        _ = self;
+        _ = key;
+        _ = value;
+    }
+};
